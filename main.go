5876148a5fdcf7a9cdd31c810fe8ab11e02980cf
aydina19 <aydina19@itu.edu.tr>
2022-05-09 21:09:59 +0300

    add config and main

--- main.go
+++ main.go
@@ -1,5 +1,25 @@
 package main
 
-func main() {
+import (
+	"aydin-tracker/aydinins-backend/pkg/common/db"
+	"aydin-tracker/aydinins-backend/pkg/controller"
 
+	"github.com/gin-gonic/gin"
+	"github.com/spf13/viper"
+)
+
+func main() {
+	viper.SetConfigFile("./pkg/common/envs/.env")
+	viper.ReadInConfig()
+
+	port := viper.Get("PORT").(string)
+
+	dbUrl := viper.Get("DB_URL").(string)
+
+	r := gin.Default()
+
+	h := db.Init(dbUrl)
+
+	controller.RouteListener(r, h)
+	r.Run(port)
 }
